270a8ba1d0f1463572429124b3b93060a3995161
Nada Khaled <nadaelkhamy@gmail.com>
2020-09-02 00:35:53 +0200

    Added NVIC library

--- Micro.h
+++ Micro.h
@@ -16,7 +16,22 @@
 
 
 #define GPIO_CLOCK_ENABLE   (*((volatile uint32*) 0x400FE608))
-	
+
+                 
+/*NVIC registers*/
+#define NVIC_EN0    (*((volatile uint32*) 0xE000E100))
+#define NVIC_EN1    (*((volatile uint32*) 0xE000E104))
+#define NVIC_EN2    (*((volatile uint32*) 0xE000E108))
+#define NVIC_EN3    (*((volatile uint32*) 0xE000E10C))
+#define NVIC_EN4    (*((volatile uint32*) 0xE000E110))
+
+#define NVIC_DIS0   (*((volatile uint32*) 0xE000E180))
+#define NVIC_DIS1   (*((volatile uint32*) 0xE000E184))
+#define NVIC_DIS2   (*((volatile uint32*) 0xE000E188))
+#define NVIC_DIS3   (*((volatile uint32*) 0xE000E18C))
+#define NVIC_DIS4   (*((volatile uint32*) 0xE000E190))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* Ports Base addreses */
 #define PORTA		0x40004000
 #define PORTB 		0x40005000
@@ -46,7 +61,7 @@
 #define GPIO_ADCCTLR_OFFSET     0x530
 #define GPIO_DMACTLR_OFFSET     0x534
 
-/*Interrupt registers*/ 
+/*Interrupt registers offsets*/ 
 #define GPIO_ISR_OFFSET         0x404
 #define GPIO_IBER_OFFSET        0x408
 #define GPIO_IEVR_OFFSET        0x40C
@@ -56,8 +71,6 @@
 #define GPIO_MISR_OFFSET         0x418
 
 #define GPIO_ICR_OFFSET          0x41C
-
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #endif /*__MICRO__H__ */
---
+++ NVIC.c
@@ -0,0 +1,76 @@
+/******************************************************************************
+ *
+ * Module: Nested Vector Interrupt Control
+ *
+ * File Name: NVIC.C
+ *
+ * Description: Source file for controlling NVIC
+ *******************************************************************************/
+
+ #include "NVIC.h"
+
+typedef enum { DISABLE,ENABLE} NVIC_interruptState;
+
+void NVIC_enableGlobalIrq()
+{
+    __enable_irq();
+}
+
+
+void NVIC_disableGlobalIrq()
+{
+    __disable_irq();
+}
+
+
+static void NVIC_changeRegState(NVIC_interruptState state,uint32 enablePort,uint32 disablePort,uint8 pin)
+{
+    if(state)
+        SET_BIT(enablePort,pin);
+    else
+        SET_BIT(disablePort,pin);
+
+}
+
+
+static void NVIC_changeInterruptState(IRQn_type IRQn,NVIC_interruptState state)
+{
+    uint8 pin=0;
+     if(IRQn/31==0)
+     {
+         pin=IRQn;
+         NVIC_changeRegState(state,NVIC_EN0,NVIC_DIS0,pin);
+     }
+     else if(IRQn/63==0)
+     {
+         pin=IRQn-32;
+         NVIC_changeRegState(state,NVIC_EN1,NVIC_DIS1,pin);
+
+     }
+     else if(IRQn/95==0)
+     {
+         pin=IRQn-64;
+         NVIC_changeRegState(state,NVIC_EN2,NVIC_DIS2,pin);
+     }
+     else if(IRQn/127==0)
+     {
+         pin=IRQn-95;
+         NVIC_changeRegState(state,NVIC_EN3,NVIC_DIS3,pin);
+     }
+     else
+     {
+         pin=IRQn-128;
+         NVIC_changeRegState(state,NVIC_EN4,NVIC_DIS4,pin);
+     }
+}
+
+
+ void NVIC_enableIRQ(IRQn_type IRQn)
+ {
+     NVIC_changeInterruptState(IRQn,ENABLE);
+ }
+
+ void NVIC_disableIRQ(IRQn_type IRQn)
+ {
+     NVIC_changeInterruptState(IRQn,DISABLE);
+ }
